c3018d64a5037392d4268c153b9da4ebf3ad5acd
argenis <argenisdavid212@gmail.com>
2025-11-24 09:50:08 -0500

    second_commit

--- Modulo1_Semana 3/Inventario_Libros.py
+++ Modulo1_Semana 3/Inventario_Libros.py
@@ -1,98 +1,188 @@
-from colorama import Fore, Back, Style, init
-init(autoreset=True)  # Inicializa colorama para que los colores se reseteen automáticamente
 
-# Inventario inicial de libros
+import datetime
+import heapq
+from itertools import groupby
+
+
 inventory = [
-    {"title": "it", "price": 10.0, "quantity": 100},
-    {"title": "the diary of anne frank", "price": 15.0, "quantity": 50},
-    {"title": "the portrait of markov", "price": 20.0, "quantity": 30},
-    {"title": "pride and prejudice", "price": 25.0, "quantity": 10},
-    {"title": "frankenstein", "price": 30.0, "quantity": 5}
+    {"title": "it", "price": 10.0, "author" : "stephen king", "category" : "horror", "quantity": 100},
+    {"title": "the shining","author" : "stephen king","category" : "horror", "price": 15.0, "quantity": 50},
+    {"title": "the portrait of dorian gray","author": "oscar wilde","category" : "gothic horror", "price": 20.0, "quantity": 30},
+    {"title": "pride and prejudice","author" : "jane austen","category" : "romance", "price": 25.0, "quantity": 10},
+    {"title": "frankenstein","author" : "mary shelley","category" : "gothic fiction", "price": 30.0, "quantity": 5}
 ]
-Amount = 0  # Variable global para cantidad de libros a agregar
+sales_inventory = [ {"client" : "","selled product" : "","quantity" : 0,"date" : "","discount" : 0}
+                       ]
+Amount = 0  # global variable for the quantity of books to add
 
-# Función para agregar libros
-def agregar():
-    book = []  # Lista temporal para almacenar el libro (no se agrega al inventario)
+# Function to register books
+def register():
+    book = []  # temporal list that ultimately is added into the inventory
     try:
-        Amount = int(input(Fore.BLUE + "¿Cuántos libros quieres agregar al sistema?: "))
+        Amount = int(input("¿Cuántos libros quieres agregar al sistema?: "))
     except ValueError:
-        print(Fore.RED + "ERROR: Valor ingresado no válido.")
+        print("ERROR: Valor ingresado no válido.")
         return
-    while Amount != 0:  # Bucle para agregar múltiples libros
+    if not Amount:
+        print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+        return
+        
+    while Amount != 0:  # loop to add multiple books
+        title = input("Cual es el titulo del libro?: ")
+        author = input("Cual es el autor del libro?")
+        category = input("Cual es la categoria del libro?: ")
+
         try:
-            title = input("Cual es el titulo del libro?: ")
             price = float(input("Cual es el precio del libro?: "))
             quantity = int(input("Cual es la cantidad del libro en stock?: "))
         except ValueError:
-            print(Fore.RED + ("ERROR, VALOR INGRESADO NO VALIDO"))
+            print("ERROR, VALOR INGRESADO NO VALIDO")
             return
-        if(price <= 0 or quantity <= 0):  # Validación de números positivos
-            print(Fore.RED+("ERROR, SOLO SE PERMITEN NUMEROS POSITIVOS"))
+        if(price <= 0 or quantity <= 0):  # positive numbers validation
+            print(("ERROR, SOLO SE PERMITEN NUMEROS POSITIVOS"))
             continue
+        if not title or not author or not category:
+            print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+            return
         else:
-            print(Fore.GREEN+("Libro introducido, agregado al sistema exitosamente ✔"))
+            print("Libro introducido, agregado al sistema exitosamente ✔")
             Amount -= 1
-            book = {"title": title, "price": price, "quantity": quantity}
-            inventory.append(book)  #Agrega el libro introducido por el usuario al sistema.
+            book = {"title": title,"author" : author, "price": price, "category" : category, "quantity": quantity}
+            inventory.append(book)  #Adds the book, entered by the user onto the inventory.
 
-# Función para consultar libros
-def consultar():
-    busqueda = input(Fore.BLUE+("Cual es el titulo del libro que buscas: "))
+# Function to consult books
+def consult():
+    search = input(("Cual es el titulo del libro que buscas: "))
+    if not search:
+        print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+        return
     for book in inventory:
-        if book["title"].lower() == busqueda.lower():  # Ignora mayúsculas/minúsculas
-            print(f"Titulo: {book['title']} | Precio: {book['price']} | Cantidad en stock: {book['quantity']}")
+        if book["title"].lower() == search.lower():  # Ignora mayúsculas/minúsculas
+            print(f"Titulo: {book['title']} |Autor: {book["author"]}| Precio: {book['price']} | Categoria: {book['category']} |Cantidad en stock: {book['quantity']}")
             return
     else:
-        print(Fore.RED + ("El libro que ingresaste no esta actualmente en el inventario"))
+        print(("El libro que ingresaste no esta actualmente en el inventario"))
 
-# Función para actualizar el precio de un libro
-def Actualizar():
-    busqueda = input(Fore.BLUE+("Cual es el titulo del libro que actualizaras: "))
-    encontrado = False
+# Function to update the book's price
+def update():
+    search = input(("Cual es el titulo del libro que actualizaras: "))
+    if not search:
+        print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+        return
+    found = False
     for book in inventory:
-        if book["title"].lower() == busqueda.lower():
-            encontrado = True
+        if book["title"].lower() == search.lower():
+            found = True
             try:
-                nuevo_precio = float(input(Fore.BLUE+(f"Ingresa el precio nuevo de {book['title']}: ")))
+                new_price= float(input((f"Ingresa el precio nuevo de {book['title']}: ")))
             except ValueError:
-                print(Fore.RED + ("ERROR, VALOR INGRESADO NO VALIDO"))
+                print(("ERROR, VALOR INGRESADO NO VALIDO"))
                 return
-            if(nuevo_precio <= 0):
-                print(Fore.RED+("ERROR, SOLO SE PERMITEN NUMEROS POSITIVOS"))
+            if(new_price<= 0):
+                print(("ERROR, SOLO SE PERMITEN NUMEROS POSITIVOS"))
+                return
+            elif not new_price:
+                print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
                 return
             else:
-                book["price"] = nuevo_precio
-                print(Fore.GREEN+("Libro actualizado exitosamente ✔"))
-    if not encontrado:  #Mensaje de error al no encontrar al libro.
-        print(Fore.RED+("Libro no encontrado en el sistema (┬┬﹏┬┬)"))
+                book["price"] = new_price
+                print(("Libro actualizado exitosamente ✔"))
+    if not found:  #error message for not finding the book
+        print("Libro no encontrado en el sistema (┬┬﹏┬┬)")
 
-# Función para eliminar libros por título
-def eliminar():
-    busqueda = input(Fore.BLUE+("Cual es el titulo del libro que eliminaras: "))
-    encontrado = [book for book in inventory if book["title"].lower() == busqueda.lower()]
-    if not encontrado:
-        print(Fore.RED + "LIBRO NO ENCONTRADO EN EL SISTEMA")
+# Function to remove book by title
+def remove():
+    search = input(("Cual es el titulo del libro que eliminaras: "))
+    found = [book for book in inventory if book["title"].lower() == search.lower()]
+    if not found:
+        print("LIBRO NO ENCONTRADO EN EL SISTEMA")
+        return
+    if not search:
+        print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
         return
     while True:
-        delete = input(Fore.BLUE + f"Se encontraron {len(encontrado)} libro(s) con el título '{busqueda}'. ¿Deseas eliminarlos todos? (Y/N): ").lower().strip()
+        delete = input(f"Se encontraron {len(found)} libro(s) con el título '{found}'. ¿Deseas eliminarlos todos? (Y/N): ").lower().strip()
         if delete == "y":
-            for book in encontrado:
-                inventory.remove(book)  # Elimina todos los libros encontrados
-            print(Fore.GREEN + f"Se eliminaron {len(encontrado)} libro(s) correctamente.")
+            for book in found:
+                inventory.remove(book)  # Deletes every book that it finds.
+            print(f"Se eliminaron {len(found)} libro(s) correctamente.")
             break
         elif delete == "n":
-            print(Fore.YELLOW + "No se eliminó ningún libro.")
+            print("No se eliminó ningún libro.")
             break
         else:
-            print(Fore.RED + "INGRESA UN VALOR ENTRE Y/N")
+            print("INGRESA UN VALOR ENTRE Y/N")
 
-# Función para calcular el valor total del inventario
-def Calcular():
-    total = sum(p["price"] * p["quantity"] for p in inventory)  # Multiplica precio por cantidad
-    print(Fore.GREEN + f"El valor total del inventario es: {total:.2f}")
+def sales_CRUD():
+    # Function to register books
 
-# Menú principal del programa
+    Amount = input("Cuantas ventas haras hoy: ")
+    if not Amount:
+        print( ("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+        return
+    sale = []  # temporal list that ultimately is added into the inventory
+
+    while Amount != 0:  # loop to add multiple books
+
+        search = input(("Cual es el titulo del libro que venderas: "))
+        for iteration in inventory:
+            if iteration["title"].lower() == search.lower():  # Ignora mayúsculas/minúsculas
+                continue
+            else:
+                print(("El libro que ingresaste no esta actualmente en el inventario"))
+                return
+        client = input("Cual es el nombre del cliente: ")
+        try:
+            quantity = int(input("Cual es la cantidad del producto vendido?: "))
+            discount = float(input("Cual es el descuento aplicado a la compra: "))
+        except ValueError:
+            print(("ERROR, VALOR INGRESADO NO VALIDO"))
+            return
+        if(discount <= 0 or quantity <= 0):  # positive numbers validation
+            print(("ERROR, SOLO SE PERMITEN NUMEROS POSITIVOS"))
+            continue
+        if(iteration["quantity"] <= 0):
+            print("EL STOCK DEL PRODUCTO ESTA VACIO")
+            break
+        if not client or not quantity or not discount or not search:
+            print(("ERROR, NO PUEDES DEJAR LOS CAMPOS VACIOS"))
+            return
+        else:
+            print(("Venta introducida, agregada al sistema exitosamente✔"))
+            Amount -= 1
+            sale = {"client" : client,"selled product" : search, "author" : iteration["author"], "quantity" : quantity,"date" : datetime.date,"discount" : discount}
+            iteration["quantity"] -= quantity
+            inventory.append(sale)  # adds the book, entered by the user onto the inventory.
+
+
+# Function to calculate the total value of the inventory
+def Calculate():
+    total = sum(p["price"] * p["quantity"] for p in inventory)  # Multiply price by quantity
+    def majorThree():
+        for iteracion in sales_inventory:
+            quantity = int(iteracion("quantity"))
+        major_three = heapq.nlargest(3, quantity)
+        print(f"Los 3 números mayores son: {major_three}")
+    def report():
+        author_list = []
+        for categoria, grupo in groupby(sales_inventory, key= sales_inventory["author"]):
+            authord = authord.append(categoria)
+            total_author = sum(authord["quantity"]*inventory["price"])
+            author_list = {"author" : sales_inventory["author"], "total" : total_author}
+            author_list.append(author_list)
+            print(f"author : {author_list["author"]}, total : {author_list['total']}")
+
+    def apply_discount():
+        for keys, values in sales_inventory:
+            net = sum((values["discount"]* total) - total)
+            net = sum(net)
+        return net
+    majorThree()
+    print(f"El valor total del inventario (ingreso bruto) es: {total:.2f}")
+    print(f"El valor total del inventario (ingreso neto) es: {apply_discount():.2f}")
+
+
+# main menu of the program.
 while True:
     try:
         option = int(input("""  
@@ -100,24 +190,27 @@ while True:
 2. Consultar libros al inventario  
 3. Actualizar precios del libro 
 4. Eliminar libro del inventario
-5. Calcular el valor total del inventario  
+5. Calcular el valor total del inventario
+6. Inventario de ventas  
 6. Salir del programa  
 Elige una opción: """))
     except ValueError:
-        print(Fore.RED + " ERROR: Debes ingresar un número.")
+        print(" ERROR: Debes ingresar un número.")
         continue
 
     if option == 1:
-        agregar()
+        register()
     elif option == 2:
-        consultar()
+        consult()
     elif option == 3:
-        Actualizar()
+        update()
     elif option == 4:
-        eliminar()
+        remove()
     elif option == 5:
-        Calcular()
+        Calculate()
     elif option == 6:
+        sales_CRUD()
+    elif option == 7:
         print("👋 Saliendo del programa...")
         break
     else:
